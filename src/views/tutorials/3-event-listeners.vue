008bd531325100a129c02c3ef8070fbd043d18e6
jinheonyoon <yjinheon@gmail.com>
2023-04-22 17:46:03 +0900

    wip

---
+++ src/views/tutorials/3-event-listeners.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "3-event-listeners.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/tutorials/helloworld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hellooworld">
+  <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // component options
+  // declare some reactive state here.
+  data() {
+    return {
+      message : 'Hello World!'
+    }
+  }
+}
+</script>
+
